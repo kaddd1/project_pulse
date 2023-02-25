c6a9e6f9a2dea5ab3663f9f389c502c4b7ba3da5
Artem <akim13919@gmail.com>
2023-02-25 19:36:00 +0500

    modalwindows

--- js/script.js
+++ js/script.js
@@ -53,4 +53,24 @@ $(document).ready(function(){
 
     toggleSlide('.catalog-item__link');
     toggleSlide('.catalog-item__back');
+
+    //model
+    $('[data-modal=consultation]').on('click', function() {
+        $('.overlay, #consultation').fadeIn('fast');
+    });
+
+    $('.modal__close').on('click', function() {
+        $('.overlay, #consultation, #order, #thanks').fadeOut('fast');
+    });
+
+    $('.button_mini').on('click', function() {
+        $('.overlay, #order').fadeIn('fast');  
+    })
+
+    $('.button_mini').each(function(i) {
+        $(this).on('click', function() {
+            $('#order .modal__text').text($('.catalog-item__subtitle').eq(i).text());
+            $('.overlay, #order').fadeIn('fast');  
+        });
+    });
 });
