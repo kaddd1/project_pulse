4dfea7fa6b376a20ef14ed2fe82f6656916347b2
Artem <akim13919@gmail.com>
2023-03-04 20:27:24 +0500

    justfixsomeerrors

--- js/script.js
+++ js/script.js
@@ -125,10 +125,13 @@ $(document).ready(function(){
         }
     });
 
-    $('a[href^="#"]').click(function() {
+    $('a[href^="#up"]').click(function() {
         $('html, body').animate({
             scrollTop: $('[name="' + $.attr(this, 'href').substr(1) + '"]').offset().top
         }, 500);
         return false;
     });
+
+    //wow plugin
+    new WOW().init();
 });
